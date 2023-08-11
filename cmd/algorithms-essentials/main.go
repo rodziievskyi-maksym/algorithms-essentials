0eaed7d7e5c46a5cf2bca920220e5dac8f72107e
max-rodziyevsky <rodziyevskydev@gmail.com>
2023-08-11 11:22:57 +0300

    Solved the problem 680 valid palindrome 2

--- cmd/algorithms-essentials/main.go
+++ cmd/algorithms-essentials/main.go
@@ -6,30 +6,28 @@ import (
 )
 
 func main() {
-	//if err := run(); err != nil {
-	//	log.Fatalf("application returned error: %s", err)
-	//}
-
 	run()
 }
 
 func run() {
 	printProcessingMessage()
 
-	//easy
+	//easy arrays and hashing
 	//arrays_and_hashing_easy.ContainsDuplicate()
 	//arrays_and_hashing_easy.ValidAnagram()
 	//arrays_and_hashing_easy.TwoSums()
 	//arrays_and_hashing_easy.MissingNumber()
 
-	// Medium
+	// Medium arrays and hashing
 	//arrays_and_hashing_medium.GroupAnagrams()
 
 	//stack.ValidParentheses()
 
 	//bit_manipulations.MissingNumber()
 
-	two_pointers.ValidPalindrome()
+	// easy two pointers
+	//two_pointers.ValidPalindrome()
+	two_pointers.ValidPalindrome2()
 }
 
 func printProcessingMessage() {
---
+++ internal/two-pointers/easy/valid-palindrome-2-680.go
@@ -0,0 +1,66 @@
+package two_pointers
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func ValidPalindrome2() {
+	//Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+	//Input:
+	//s := "abcwwca"
+	//Output: true
+
+	//Input:
+	s2 := "abca"
+	//Output: true
+	//Explanation: You could delete the character 'c'.
+
+	//fmt.Println(validPalindrome(s))
+	fmt.Println(validPalindrome(s2))
+}
+
+func validPalindrome(s string) bool {
+	// two pointers
+	if len(s) <= 2 {
+		return true
+	}
+
+	leftIndex := 0
+	rightIndex := len(s) - 1
+
+	for leftIndex < rightIndex {
+		left, right := runeUtil(rune(s[leftIndex]), rune(s[rightIndex]))
+
+		if left == right {
+			leftIndex++
+			rightIndex--
+		} else {
+			return validPalindromeUtil(s, leftIndex+1, rightIndex) || validPalindromeUtil(s, leftIndex, rightIndex+1)
+		}
+
+		return true
+	}
+
+	return true
+}
+
+func validPalindromeUtil(s string, leftIndex, rightIndex int) bool {
+	for leftIndex < rightIndex {
+		left, right := runeUtil(rune(s[leftIndex]), rune(s[rightIndex]))
+
+		if left == right {
+			leftIndex++
+			rightIndex--
+		} else {
+			return false
+		}
+	}
+
+	return true
+}
+
+func runeUtil(left, right rune) (rune, rune) {
+	return unicode.ToLower(left), unicode.ToLower(right)
+}
